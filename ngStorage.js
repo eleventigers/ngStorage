c0b41070ea12a3bf9c97b964c6e896133b7bb217
eleventigers <phonodelik@gmail.com>
2013-12-11 16:33:06 +0000

    more robust support detection

--- ngStorage.js
+++ ngStorage.js
@@ -1,6 +1,6 @@
-'use strict';
-
 (function() {
+
+    'use strict';
 
     /**
      * @ngdoc overview
@@ -38,26 +38,54 @@
                 $window,
                 $log
             ){
-                // #9: Assign a placeholder object if Web Storage is unavailable to prevent breaking the entire AngularJS app
-                var webStorage = $window[storageType] || ($log.warn('This browser does not support Web Storage!'), {}),
-                    $storage = {
-                        $default: function(items) {
-                            for (var k in items) {
-                                angular.isDefined($storage[k]) || ($storage[k] = items[k]);
-                            }
+                var webStorage, $storage, _last$storage, _debounce;
 
-                            return $storage;
-                        },
-                        $reset: function(items) {
-                            for (var k in $storage) {
-                                '$' === k[0] || delete $storage[k];
-                            }
+                function isStorageSupported(storageType) {
+                    var supported;
 
-                            return $storage.$default(items);
+                    try {
+                        supported = $window[storageType];
+                    } catch (err) {
+                        supported = false;
+                    }
+
+                    // When Safari (OS X or iOS) is in private browsing mode, it appears as though localStorage
+                    // is available, but trying to call .setItem throws an exception below:
+                    // "QUOTA_EXCEEDED_ERR: DOM Exception 22: An attempt was made to add something to storage that exceeded the quota."
+                    if (supported && storageType === 'localStorage') {
+                        var key = '__' + Math.round(Math.random() * 1e7);
+
+                        try {
+                            localStorage.setItem(key, key);
+                            localStorage.removeItem(key);
                         }
+                        catch (err) {
+                            supported = false;
+                        }
+                    }
+
+                    return supported;
+                }
+
+                // #9: Assign a placeholder object if Web Storage is unavailable to prevent breaking the entire AngularJS app
+                webStorage = isStorageSupported(storageType) || ($log.warn('This browser does not support Web Storage!'), {});
+
+                $storage = {
+                    $default: function(items) {
+                        for (var k in items) {
+                            angular.isDefined($storage[k]) || ($storage[k] = items[k]);
+                        }
+
+                        return $storage;
                     },
-                    _last$storage,
-                    _debounce;
+                    $reset: function(items) {
+                        for (var k in $storage) {
+                            '$' === k[0] || delete $storage[k];
+                        }
+
+                        return $storage.$default(items);
+                    }
+                };
 
                 for (var i = 0, k; i < webStorage.length; i++) {
                     // #8, #10: `webStorage.key(i)` may be an empty string (or throw an exception in IE9 if `webStorage` is empty)
